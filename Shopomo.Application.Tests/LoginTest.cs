5c085af8763059c64de07e0aa3fb7c7293b0e1a6
Leonardo Cadastro <Leonardo Cadastro>
2016-11-03 06:25:41 -0200

    Added unit tests

---
+++ Shopomo.Application.Tests/LoginTest.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shopomo.Application.Interfaces;
+using Shopomo.Application.App;
+using Shopomo.OTP.Domain.Services;
+using Shopomo.OTP.Domain.Interfaces.Services;
+using Rhino.Mocks;
+using Shopomo.OTP.Domain.Entities;
+using System.Collections.Generic;
+using Shopomo.OTP.Infra.Data.Repositories;
+using Shopomo.Presentation.Client.Models;
+
+namespace Shopomo.Application.Tests
+{
+    [TestClass]
+    public class LoginTest
+    {
+        private ILoginAppService _loginAppService;
+
+        public LoginTest()
+        {
+            _loginAppService = new LoginAppService(new LoginService(new LoginRepository()));
+        }
+
+        [TestMethod]
+        public void Test_Length_Password()
+        {
+            var password = _loginAppService.GenerateOTP("999", DateTime.Now);
+            Assert.IsTrue(password.Length == 6);
+        }
+
+        [TestMethod]
+        public void Test_Two_Users_Password()
+        {
+            var password = _loginAppService.GenerateOTP("999", DateTime.Now);
+            var password2 = _loginAppService.GenerateOTP("998", DateTime.Now);
+
+            Assert.AreNotEqual(password, password2);
+        }
+
+        [TestMethod]
+        public void Test_Password_Adding_Less_Than_30_Seconds()
+        {
+            var date1 = Convert.ToDateTime("03/11/2016 05:56:00");
+            var date2 = Convert.ToDateTime("03/11/2016 05:56:29");
+
+            var password = _loginAppService.GenerateOTP("999", date1);
+            var password2 = _loginAppService.GenerateOTP("999", date2);
+
+            Assert.AreEqual(password, password2);
+        }
+
+        [TestMethod]
+        public void Test_Password_Adding_More_Than_30_Seconds()
+        {
+            var date1 = Convert.ToDateTime("03/11/2016 05:56:00");
+            var date2 = Convert.ToDateTime("03/11/2016 05:56:32");
+
+            var password = _loginAppService.GenerateOTP("999", date1);
+            var password2 = _loginAppService.GenerateOTP("999", date2);
+
+            Assert.AreNotEqual(password, password2);
+        }
+
+        [TestMethod]
+        public void Test_Authenticate_Password()
+        {
+            var date1 = Convert.ToDateTime("03/11/2016 05:56:00");
+            var date2 = Convert.ToDateTime("03/11/2016 05:56:28");
+
+            var password = _loginAppService.GenerateOTP("999", date1);
+
+            var result = _loginAppService.AuthenticateOTP("999", date2, password);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_Fail_Authenticate_Password_By_UserId()
+        {
+            var date1 = Convert.ToDateTime("03/11/2016 05:56:00");
+            var date2 = Convert.ToDateTime("03/11/2016 05:56:28");
+
+            var password = _loginAppService.GenerateOTP("999", date1);
+
+            var result = _loginAppService.AuthenticateOTP("998", date2, password);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_Fail_Authenticate_Password_By_Date()
+        {
+            var date1 = Convert.ToDateTime("03/11/2016 05:56:01");
+            var date2 = Convert.ToDateTime("03/11/2016 05:56:59");
+
+            var password = _loginAppService.GenerateOTP("999", date1);
+
+            var result = _loginAppService.AuthenticateOTP("999", date2, password);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_Fail_Authenticate_Password_By_Wrong_Password()
+        {
+            var date2 = Convert.ToDateTime("03/11/2016 05:56:32");
+
+            var password = "test";
+
+            var result = _loginAppService.AuthenticateOTP("999", date2, password);
+
+            Assert.IsFalse(result);
+        }
+
+
+    }
+}
---
+++ Shopomo.Application.Tests/UserTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shopomo.Application.Interfaces;
+using Shopomo.Application.App;
+using Shopomo.OTP.Domain.Services;
+using Shopomo.OTP.Domain.Interfaces.Services;
+using Rhino.Mocks;
+using Shopomo.OTP.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Shopomo.Application.Tests
+{
+    [TestClass]
+    public class UserTest
+    {
+        private IUserAppService _userAppService;
+
+        public UserTest()
+        {
+            var user1 = new User { UserId = 998, Email = "cadastro@gmail.com", Name = "Cadastro" };
+            var user2 = new User { UserId = 999, Email = "leonardo@gmail.com", Name = "Leonardo" };
+
+            var users = new List<User>() {user1, user2};
+
+            var userService = MockRepository.GenerateStub<IUserService>();
+
+            userService.Stub(h => h.GetAll())
+                .IgnoreArguments()
+                .Return(users);
+
+            userService.Stub(h => h.GetById(999))
+                .IgnoreArguments()
+                .Return(user2);
+
+            _userAppService = new UserAppService(userService);
+        }
+
+        [TestMethod]
+        public void Test_GetAll_Users()
+        {
+            var users = _userAppService.GetAll();
+            Assert.IsTrue(users.Count == 2);
+        }
+
+        [TestMethod]
+        public void Test_Get_User_By_Id()
+        {
+            var user = _userAppService.GetById(999);
+            Assert.AreEqual(user.Name, "Leonardo");
+        }
+
+    }
+}
